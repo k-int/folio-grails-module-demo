8729384cfe081f9105032e39fe21fb27785d1bfd
Ian Ibbotson <ianibbo@gmail.com>
2017-09-18 01:38:49 +0100

    Working on schema per tenant demo

---
+++ folio-demo-module/src/main/groovy/OkapiTenantResolver.groovy
@@ -0,0 +1,42 @@
+import groovy.transform.CompileStatic
+import org.grails.datastore.mapping.multitenancy.TenantResolver
+import org.grails.datastore.mapping.multitenancy.exceptions.TenantNotFoundException
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletWebRequest
+
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * A tenant resolver that resolves the tenant from the request HTTP Header
+ *
+ * @author Sergio del Amo
+ * @since 6.1.7
+ */
+@CompileStatic
+class OkapiTenantResolver implements TenantResolver {
+
+    public static final String HEADER_NAME = "X-Okapi-Tenant"
+
+    /**
+     * The name of the header
+     */
+    String headerName = HEADER_NAME
+
+    @Override
+    Serializable resolveTenantIdentifier() {
+
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes()
+        if(requestAttributes instanceof ServletWebRequest) {
+
+            HttpServletRequest httpServletRequest = ((ServletWebRequest) requestAttributes).getRequest()
+            String tenantId = httpServletRequest.getHeader(headerName.toLowerCase())
+
+            if ( tenantId ) {
+                return tenantId
+            }
+            throw new TenantNotFoundException("Tenant could not be resolved from HTTP Header: ${headerName}")
+        }
+        throw new TenantNotFoundException("Tenant could not be resolved outside a web request")
+    }
+}
