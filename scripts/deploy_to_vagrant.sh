3719a5be2cd9f39b86a9945ddd8e23b11ebb6489
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-09-25 17:12:43 +0100

    More tidying

--- scripts/deploy_to_vagrant.sh
+++ scripts/deploy_to_vagrant.sh
@@ -28,19 +28,19 @@ echo $OPTIONS
 # scp -F /tmp/cfg.txt $script_base/../folio-demo-module/build/ModuleDescriptor.json default:/etc/folio/module-descriptors/mod-grails-demo
 
 # Refine - step 1 : Copy files to vagrant user
-scp -F /tmp/cfg.txt $script_base/../folio-demo-module/build/libs/folio-demo-module-0.0.1.war default:
+scp -F /tmp/cfg.txt $script_base/../folio-demo-module/build/libs/mod-grails-demo-0.0.1.war default:
 scp -F /tmp/cfg.txt $script_base/../folio-demo-module/build/DeploymentDescriptor.json default:
 scp -F /tmp/cfg.txt $script_base/../folio-demo-module/build/ModuleDescriptor.json default:
 scp -F /tmp/cfg.txt $script_base/../attic/folio_globals_vagrant.yaml default:folio_globals.yaml
 
 vagrant ssh -c "sudo mv DeploymentDescriptor.json /etc/folio/deployment-descriptors/mod-grails-demo.json"
 vagrant ssh -c "sudo mv ModuleDescriptor.json /etc/folio/module-descriptors/mod-grails-demo.json"
-vagrant ssh -c "sudo mv folio-demo-module-0.0.1.war /home/folio/folio-demo-module.war"
+vagrant ssh -c "sudo mv mod-grails-demo-0.0.1.war /home/folio/mod-grails-demo.war"
 vagrant ssh -c "sudo mv folio_globals.yaml /home/folio/folio_globals.yaml"
 
 echo CHOWN phase
 # Chown all the files just copied to folio:folio
-vagrant ssh -c "sudo chown folio:folio /etc/folio/deployment-descriptors/mod-grails-demo.json /etc/folio/module-descriptors/mod-grails-demo.json /home/folio/folio-demo-module.war /home/folio/folio_globals.yaml"
+vagrant ssh -c "sudo chown folio:folio /etc/folio/deployment-descriptors/mod-grails-demo.json /etc/folio/module-descriptors/mod-grails-demo.json /home/folio/mod-grails-demo.war /home/folio/folio_globals.yaml"
 
 echo deploy module
 vagrant ssh -c "curl -i -w '\n' -X POST -H 'Content-type: application/json' -d @/etc/folio/module-descriptors/mod-grails-demo.json http://localhost:9130/_/proxy/modules"
