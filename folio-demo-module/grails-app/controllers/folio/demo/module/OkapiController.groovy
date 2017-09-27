ba6de5a3abc33d3ed519bba6f77d17e3e94e1a66
Ian Ibbotson <ian.ibbotson@gmail.com>
2017-09-27 11:06:44 +0100

    Thinking about DELETE on tenant resource

--- folio-demo-module/grails-app/controllers/folio/demo/module/OkapiController.groovy
+++ folio-demo-module/grails-app/controllers/folio/demo/module/OkapiController.groovy
@@ -14,18 +14,34 @@ class OkapiController {
   static responseFormats = ['json', 'xml']
   def hibernateDatastore
   def dataSource
+  def tenantAdminService
+
 
   def index() { 
   }
 
   // GET And DELETE verbs with header X-Okapi-Tenant indicate activation of this module for a given tenant.
   def tenant() {
-    println("OkapiController::tenant ${params} ${request.getHeader('X-Okapi-Tenant')}");
+    String tenant_id = request.getHeader('X-Okapi-Tenant')
+    log.info("OkapiController::tenant ${request.method} ${params} ${tenant_id}");
     def result = [:]
-
-    // Lets sort dynamic creation of this tenant
-    // HibernateDatastore datastore = ...
-    // datastore.addTenantForSchema("myNewSchema")
+    if ( tenant_id && tenant_id.trim().length() > 0 ) {
+      switch ( request.method ) {
+        case 'GET':
+        case 'POST':
+          tenantAdminService.createTenant();
+          break;
+        case 'DELETE':
+          log.debug("Request to destroy tenant -- hanging fire here");
+          break;
+        default:
+          log.warn("Unhandled verb ${request.method} for module /_/tenant endpoint");
+          break;
+      }
+    }
+    else {
+      log.error("No X-Okapi-Tenant header");
+    }
 
     render result as JSON
   }
